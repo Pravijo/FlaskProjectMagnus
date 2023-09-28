48b3d25b480896c4a0488b85e06383466337947d
Pravijo <mpravalika89@gmail.com>
2023-09-27 21:12:19 -0400

    Flask Examples

---
+++ FlaskEx.py
@@ -0,0 +1,23 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/home")
+def home():
+    return "Welcome to web application using Flask"
+
+
+@app.route("/help")
+def help():
+    return "This is Help Page using Flask"
+
+
+def contact():
+    return "This is Contact page using  the Flask"
+
+
+app.add_url_rule("/contact","contact",contact)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
